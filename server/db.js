510033ede3f46f83c4155b4a70ffb8f0ab55b73e
elvisespinozaN <elvis.espinoza.navarrete@outlook.com>
2025-04-17 21:32:48 -0400

    create users, products, and favorites tables with foreign key constraints

--- server/db.js
+++ server/db.js
@@ -4,4 +4,32 @@ const client = new pg.Client(
   process.env.DATABASE_URL || "postgres://localhost/acme_store"
 );
 
-module.exports = { client };
+async function createTables() {
+  const SQL = `
+    DROP TABLE IF EXISTS favorites;
+    DROP TABLE IF EXISTS users;
+    DROP TABLE IF EXISTS products;
+
+    CREATE TABLE users(
+      id UUID PRIMARY KEY,
+      username VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL
+    );
+
+    CREATE TABLE products(
+      id UUID PRIMARY KEY,
+      name VARCHAR(255) NOT NULL
+    );
+
+    CREATE TABLE favorites(
+      id UUID PRIMARY KEY,
+      product_id UUID REFERENCES products(id) NOT NULL,
+      user_id UUID REFERENCES users(id) NOT NULL,
+      CONSTRAINT unique_user_product UNIQUE (user_id, product_id)
+    );
+  `;
+
+  await client.query(SQL);
+}
+
+module.exports = { client, createTables };
--- server/seed.js
+++ server/seed.js
@@ -1,5 +1,13 @@
-const { client } = require("./db");
+const { client, createTables } = require("./db");
 
 async function seed() {
+  console.log("- Seeeding database");
   await client.connect();
+
+  await createTables();
+  console.log("- Tables created");
+
+  await client.end();
 }
+
+seed();
