bc5b9c78efe4dda2c7adfb8c864f7210bc3ea399
elvisespinozaN <elvis.espinoza.navarrete@outlook.com>
2025-04-19 13:10:43 -0400

    api routes and postman crud verification

--- server/index.js
+++ server/index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const morgan = require("morgan");
-const { client } = require("./db");
+const {
+  client,
+  fetchUsers,
+  fetchProducts,
+  fetchFavorites,
+  createFavorite,
+  destroyFavorite,
+} = require("./db");
 
 const server = express();
 client.connect();
@@ -10,3 +17,49 @@ server.use(morgan("dev"));
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => console.log(`server listening on port ${port}`));
+
+// routes
+server.get("/api/users", async (req, res, next) => {
+  try {
+    const users = await fetchUsers();
+    res.json(users);
+  } catch (e) {
+    next(e);
+  }
+});
+
+server.get("/api/products", async (req, res, next) => {
+  try {
+    const products = await fetchProducts();
+    res.json(products);
+  } catch (e) {
+    next(e);
+  }
+});
+
+server.get("/api/users/:id/favorites", async (req, res, next) => {
+  try {
+    const favorites = await fetchFavorites(req.params.id);
+    res.json(favorites);
+  } catch (e) {
+    next(e);
+  }
+});
+
+server.post("/api/users/:id/favorites", async (req, res, next) => {
+  try {
+    const favorite = await createFavorite(req.params.id, req.body.product_id);
+    res.status(201).json(favorite);
+  } catch (e) {
+    next(e);
+  }
+});
+
+server.delete("/api/users/:userId/favorites/:id", async (req, res, next) => {
+  try {
+    await destroyFavorite(req.params.id, req.params.userId);
+    res.sendStatus(204);
+  } catch (e) {
+    next(e);
+  }
+});
